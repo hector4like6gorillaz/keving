37aca5dda4462c50fda3dc89b9edd76d1714cbbb
Hector4like6gorillaz <78667228+hector4like6gorillaz@users.noreply.github.com>
2023-09-14 15:30:55 -0600

    changes

--- src/modules/localStorage/LocalStorageModule.tsx
+++ src/modules/localStorage/LocalStorageModule.tsx
@@ -3,12 +3,24 @@ import Button from "../../components/button/Button";
 
 const nombreVars = ["valor", "contador"];
 
+const valores = {
+  name: "hector",
+  last: "balan",
+  email: "hwctor@gmail.com",
+  age: 34,
+  role: "admin",
+};
+
 const LocalStorageModule = () => {
   const [valor, setvalor] = useState(0);
+  const [form, setform] = useState(valores);
 
   //localStorage.getItem(“key”);
-  const saveInLocalStorage = (varName: string, val: number) =>
+  const saveInLocalStorage = (varName: string, val: any) =>
     localStorage.setItem(varName, `${val}`);
+
+  const saveJsonInLocalStorage = (varName: string, val: any) =>
+    localStorage.setItem(varName, val);
   //localStorage.getItem(“key”);
   const saveInSesionStorage = (varName: string, val: number) =>
     sessionStorage.setItem(varName, `${val}`);
@@ -26,11 +38,27 @@ const LocalStorageModule = () => {
     }
   }, []);
 
+  const getJson = () => {
+    const name = "form-user";
+    if (getLocalStorage(name) !== null) {
+      return JSON.parse(getLocalStorage(name) || "");
+    }
+    return null;
+  };
+
+  console.log(getJson());
+
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <Button
         text="guardar en local storage"
         onClick={() => saveInLocalStorage(nombreVars[1], valor)}
+      />
+      <Button
+        text="guardar form en local storage"
+        onClick={() =>
+          saveJsonInLocalStorage("form-user", JSON.stringify(form))
+        }
       />
       <Button
         text="guardar en session storage"
